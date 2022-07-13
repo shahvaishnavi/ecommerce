55cd93e04c73ac6f218ee5c5ec2b0ede40a812dd
demo <vaishu191202@gmail.com>
2022-07-13 15:33:23 +0530

    Y

--- lib/main.dart
+++ lib/main.dart
@@ -61,9 +61,55 @@ class _addproductState extends State<addproduct> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigoAccent,
+      backgroundColor: Colors.blueGrey,
+      body: SafeArea(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                showDialog(
+                    builder: (context) {
+                      return Dialog(
+                        child: Column(children: [
+                          ElevatedButton.icon(
+                              onPressed: () {},
+                              icon: Icon(Icons.camera),
+                              label: Text("camera"))
+                        ]),
+                      );
+                    },
+                    context: context);
+              },
+              child: Container(),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 50, 20, 30),
+              child: Center(
+                child: TextField(
+                  controller: name,
+                  decoration: InputDecoration(
+                      labelText: "ENTER UR NAME", hintText: "NAME"),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 20, 20, 30),
+              child: Center(
+                child: TextField(
+                  controller: price,
+                  decoration:
+                      InputDecoration(labelText: "SET PRICE", hintText: "RATE"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  TextEditingController name = TextEditingController();
+  TextEditingController price = TextEditingController();
 }
 
 class viewproduct extends StatefulWidget {
